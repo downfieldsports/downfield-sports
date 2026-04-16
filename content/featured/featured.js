d10ea74647d344a57d9b2207092eabb8babd186e
downfieldsports <mchakiry@icloud.com>
2026-04-15 21:53:28 -0400

    Add files via upload

--- content/featured/featured.js
+++ content/featured/featured.js
@@ -2,7 +2,7 @@
 // Required fields: href, image, alt, publishedAt (YYYY-MM-DD), title, summary
 window.dfsFeatured = {
   href: "articles/Big-Changes.html",
-  image: "Images/Article Images/Football+Field.png",
+  image: "/Images/Article Images/Football+Field.png",
   alt: "Football field with players",
   publishedAt: "2026-04-14",
   title: "Big Changes: Getting Ready for the 2026 NFL Season",
